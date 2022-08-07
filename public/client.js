bc72e432fc848ee607386153ff6c7ef2fcacf78d
Charlie Taylor <peekoecode@gmail.com>
2022-08-06 22:40:01 -0700

    scrap cube, it's cabin time

---
+++ public/client.js
@@ -0,0 +1,79 @@
+import * as THREE from 'three'
+import { OrbitControls } from './jsm/controls/OrbitControls.js'
+import Stats from './jsm/libs/stats.module.js'
+import { GUI } from './jsm/libs/lil-gui.module.min.js'
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.z = 2
+
+const ambientLight = new THREE.AmbientLight(0x404040, 10); // soft white light
+scene.add(ambientLight);
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const loader = new GLTFLoader()
+loader.load(
+    'models/cabin.gltf',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+                const m = child
+                m.receiveShadow = true
+                m.castShadow = true
+            }
+            if (child.isLight) {
+                const l = child
+                l.castShadow = true
+                l.shadow.bias = -0.003
+                l.shadow.mapSize.width = 2048
+                l.shadow.mapSize.height = 2048
+            }
+        })
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+window.addEventListener(
+    'resize',
+    () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    },
+    false
+)
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
+
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update()
+    render()
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
